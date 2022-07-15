0d2103a5034a73a1baf7d0bb8567892e8980397b
Gerald <21037552@myrp.edu.sg>
2022-07-16 01:27:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ndp_songs/ShowSong.java
@@ -0,0 +1,14 @@
+package com.example.ndp_songs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_song);
+    }
+}
